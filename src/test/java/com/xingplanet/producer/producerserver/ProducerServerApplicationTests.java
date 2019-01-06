20c186e37cc4ccc0b5180313ce5d3698ec3e97d5
xingplanet <wjzdyking@qq>
2019-01-06 16:27:56 +0800

    first commit

---
+++ src/test/java/com/xingplanet/producer/producerserver/ProducerServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xingplanet.producer.producerserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProducerServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
